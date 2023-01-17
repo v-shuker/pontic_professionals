e0f396c44b01cac4dc26a420fd454f055787e2f7
verity shuker <verity.shuker@gmail.com>
2023-01-17 16:05:30 +0000

    creating home about contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "home", to: "pages#home"
+  get "about", to: "pages#about"
+  get "contact", to: "pages#contact"
 end
