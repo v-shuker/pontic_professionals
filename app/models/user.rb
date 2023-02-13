cff9e0375799e6de2da453717035f7dc79b3971f
verity shuker <verity.shuker@gmail.com>
2023-02-13 15:10:26 +0000

    adding email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address" }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
